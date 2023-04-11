7b6464fdbfec5f47d5821e403c00f66ed953a292
foucaud_e <foucauden@gmail.com>
2023-04-11 14:25:57 +0200

    refactor: remove useless file

--- msx/constant.go
+++
@@ -1,5 +0,0 @@
-package msx
-
-const (
-	MSX_API = "https://api.multiversx.com/"
-)
--- msx/model.go
+++
@@ -1,6 +0,0 @@
-package msx
-
-type ErrorElrond struct {
-	StatusCode int    `json:"statusCode"`
-	Message    string `json:"message"`
-}
